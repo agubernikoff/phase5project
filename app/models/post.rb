0154571508aaf45b754a7d969759ce18c8921860
alexander gubernikoff <agubernikoff@gmail.com>
2022-05-03 23:27:29 -0400

    new post form

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,20 +5,24 @@ class Post < ApplicationRecord
   has_many :comments,dependent: :destroy
   has_many :users, through: :comments
   
-  has_many_attached :media_types
+  has_many_attached :media
 
-  validate :acceptable_media_types
+  validate :acceptable_media
 
-  def acceptable_media_types
-    return unless media_types.attached?
+  def acceptable_media
+    return unless media.attached?
 
-    unless media_types.byte_size <= 1.megabyte
-      errors.add(:media_types, "is too big")
+    unless media.byte_size <= 1.megabyte
+      errors.add(:media, "is too big")
     end
 
-    acceptable_types = ["media_types/jpeg", "media_types/png","media_types/gif","media_types/MP4","media_types/MOV"]
-    unless acceptable_types.include?(media_types.content_type)
-      errors.add(:media_types, "must be a JPEG, PNG, GIF, MOV, or MP4 file")
+    acceptable = ["media/jpeg", "media/png","media/gif","media/MP4","media/MOV"]
+    unless acceptable.include?(media.content_type)
+      errors.add(:media, "must be a JPEG, PNG, GIF, MOV, or MP4 file")
+    end
+
+    unless media.length<=10
+      errors.add(:media, "...too many attachments. Max is 10")
     end
   end
 
--- client/src/NewPost.js
+++ client/src/NewPost.js
@@ -1,7 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function NewPost() {
-  return <div>NewPost</div>;
+function NewPost({ user }) {
+  const [project_id, setProjectId] = useState(user.projects[0].id);
+  const [media, setMedia] = useState("");
+  const [caption, setCaption] = useState([]);
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
+
+  const formData = new FormData();
+  formData.append("project_id", project_id);
+  formData.append("media", media);
+  formData.append("caption", caption);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!media) setErrors(["Please upload photos or videos"]);
+    else if (media.length > 10)
+      setErrors(["Too many files. Maximum allowed is 10"]);
+    else {
+      fetch("/posts", {
+        method: "POST",
+        body: formData,
+      }).then((r) => {
+        if (r.ok) {
+          r.json().then((data) => {
+            console.log(data);
+            navigate("/");
+          });
+        } else {
+          r.json().then((data) => setErrors(data.errors));
+        }
+      });
+    }
+  }
+
+  const projectOptions = user.projects.map((p) => (
+    <option key={p.title} value={p.id}>
+      {p.title}
+    </option>
+  ));
+
+  function seperateFiles(filelist) {
+    const array = [];
+    for (let i = 0; i < filelist.length; i++) {
+      array.push(filelist[i]);
+    }
+    setMedia(array);
+  }
+
+  return (
+    <div>
+      NewPost
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "fit-content",
+          margin: "auto",
+        }}
+      >
+        <label>Choose a project to post to:</label>
+        <select onChange={(e) => setProjectId(parseInt(e.target.value))}>
+          {projectOptions}
+        </select>
+        <br />
+        <label htmlFor="media">Upload photos or videos:</label>
+        <input
+          type="file"
+          accept=".jpeg,.png,.gif,.mov,.mp4"
+          name="media"
+          multiple
+          onChange={(e) => seperateFiles(e.target.files)}
+        ></input>
+        <br />
+        <label htmlFor="caption">Caption:</label>
+        <textarea
+          type="text"
+          id="description"
+          autoComplete="off"
+          rows="10"
+          cols="75"
+          //   placeholder="Tell us about your new project"
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+        />
+        <br />
+        <input type="submit" value="Submit" />
+        {errors.map((err) => (
+          <h3
+            key={err}
+            style={{
+              display: "block",
+              margin: "auto",
+              marginTop: 10,
+              textAlign: "center",
+            }}
+          >
+            {err}
+          </h3>
+        ))}
+      </form>
+    </div>
+  );
 }
 
 export default NewPost;
