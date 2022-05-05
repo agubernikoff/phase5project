7a3d3c038f7fe08be4bb77fb7d8c94118da423b3
alexander gubernikoff <agubernikoff@gmail.com>
2022-05-05 13:15:36 -0400

    cleaned up userSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,15 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
 include Rails.application.routes.url_helpers
 
-  attributes :id,:username,:isSeller,:profile_picture,:projects,:likes
-  
-  def projects
-    ActiveModelSerializers::SerializableResource.new(object.projects,each_serializer: ProjectSerializer)
-  end
-
-  def likes
-    ActiveModelSerializers::SerializableResource.new(object.likes,each_serializer: LikeSerializer)
-  end
+  attributes :id,:username,:isSeller,:profile_picture
+  has_many :projects
+  has_many :likes
   
   def profile_picture
     rails_blob_path(object.profile_picture,only_path:true) if object.profile_picture.attached?
