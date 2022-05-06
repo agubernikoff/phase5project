b8796f9a2a828f954da8c9e94fa09eeb2bcd1f48
alexander gubernikoff <agubernikoff@gmail.com>
2022-05-06 10:38:13 -0400

    post index only return New Project posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound,with: :render_not_found
     
     def index
-        render json: Post.all
+        render json: Post.joins(:project).where(project: {status:'New Project'})
     end
 
     def show
