def8c57bc5be3a86ea340ac169ebd85230652363
alexander gubernikoff <agubernikoff@gmail.com>
2022-05-08 19:02:06 -0400

    comingsoon sorted by most recent update

--- client/src/ComingSoon.js
+++ client/src/ComingSoon.js
@@ -13,21 +13,41 @@ function ComingSoon({
   updatePostsOnLikesThreshold,
   updateProjectsOnThreshold,
 }) {
-  const availableForPreorder = preOrderProjects.map((p) => (
-    <Preorder
-      key={p.id}
-      project={p}
-      user={user}
-      updateUserLikesOnLike={updateUserLikesOnLike}
-      updateUserLikesOnUnlike={updateUserLikesOnUnlike}
-      updatePostLikesOnLike={updatePostLikesOnLike}
-      updatePostLikesOnUnlike={updatePostLikesOnUnlike}
-      updatePostCommentsOnComment={updatePostCommentsOnComment}
-      updatePostCommentsOnDelete={updatePostCommentsOnDelete}
-      updatePostsOnLikesThreshold={updatePostsOnLikesThreshold}
-      updateProjectsOnThreshold={updateProjectsOnThreshold}
-    />
-  ));
+  const projectsWithoutUpdates = [...preOrderProjects].filter(
+    (p) => !p.production_updates[0]
+  );
+
+  const projectsWithUpdates = [...preOrderProjects].filter(
+    (p) => p.production_updates[0]
+  );
+
+  const sorted = [...projectsWithUpdates].sort(
+    (a, b) =>
+      new Date(
+        b.production_updates[b.production_updates.length - 1].created_at
+      ).getTime() -
+      new Date(
+        a.production_updates[a.production_updates.length - 1].created_at
+      ).getTime()
+  );
+
+  const availableForPreorder = [...sorted, ...projectsWithoutUpdates].map(
+    (p) => (
+      <Preorder
+        key={p.id}
+        project={p}
+        user={user}
+        updateUserLikesOnLike={updateUserLikesOnLike}
+        updateUserLikesOnUnlike={updateUserLikesOnUnlike}
+        updatePostLikesOnLike={updatePostLikesOnLike}
+        updatePostLikesOnUnlike={updatePostLikesOnUnlike}
+        updatePostCommentsOnComment={updatePostCommentsOnComment}
+        updatePostCommentsOnDelete={updatePostCommentsOnDelete}
+        updatePostsOnLikesThreshold={updatePostsOnLikesThreshold}
+        updateProjectsOnThreshold={updateProjectsOnThreshold}
+      />
+    )
+  );
   return <div>ComingSoon{availableForPreorder}</div>;
 }
 
